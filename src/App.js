81225e12418a6107e8e5f64aa9321a363e70cac9
ClemenceDelestre <delestreclemence@gmail.com>
2021-12-10 12:04:08 +0100

    useState ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './heartrate.svg';
 import './App.css';
 import Heartrate from './compenents/Heartrate';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  let rythmeCardiaque = 0; 
-  
+  const [rythCardiaque, setRythCardiaque] = useState(0);
+
   useEffect(() => {
     setInterval(() => {
-      rythmeCardiaque = 40 + Math.floor(Math.random() * (200 - 40));
+      setRythCardiaque(40 + Math.floor(Math.random() * (200 - 40)));
     }, 4000); //délai d'exécution du callback
-  },[]) //UseEffect est appelé que quand le composant est crée
+  }, []); //UseEffect est appelé que quand le composant est crée
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Les battements du coeur en temps réel</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <Heartrate data={rythmeCardiaque} />
+        <Heartrate data={rythCardiaque} />
       </header>
     </div>
   );
--- src/compenents/Heartrate.js
+++ src/compenents/Heartrate.js
@@ -1,15 +1,14 @@
 function Heartrate(props) {
-  const { data } = props; //mettre les valeurs de l'App dans le composant
-  console.log(data);
-  //var rythmeCardiaque = 40 + Math.floor(Math.random() * (200 - 40));
-  var pbBpm = '';
-  var description = ' ';
-  if (data < 50 || data > 150) {
-    pbBpm = 'pbBpm';
-    description = data + ' -> Danger';
-  } else {
-    pbBpm = 'bpmOk';
-    description = data + ' -> Ok';
+  const {data} = props ; //mettre les valeurs de l'App dans le composant
+  var pbBpm = ""
+  var description = " "
+  if (data<50 || data>150) {
+     pbBpm = "pbBpm";
+     description = data+ " -> Danger";
+  }
+  else {
+     pbBpm ="bpmOk";
+     description = data + " -> Ok";
   }
   return <div class={pbBpm}> {description} </div>;
 }
