c4ba01a947ebd0cb2254526beeadd43476b5688f
ClemenceDelestre <delestreclemence@gmail.com>
2021-12-10 10:05:56 +0100

    Création Nouveau Composant

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <p>
           Les battements du coeur en temps réel
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </p>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p> Nouveau Composant </p>
+        
       </header>
     </div>
   );
