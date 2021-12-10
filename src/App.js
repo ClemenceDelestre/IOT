cb3fed6cca24feb9671693dde5acfc9d7e71cf0c
ClemenceDelestre <delestreclemence@gmail.com>
2021-12-10 09:57:43 +0100

    Changement paragraphe présentation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Les battements du coeur en temps réel
         </p>
         <a
           className="App-link"
