12e70a827962a5a6474659555b33fd81bd2987ac
ClemenceDelestre <delestreclemence@gmail.com>
2021-12-10 11:22:14 +0100

    modif style + pb Props

--- src/compenents/Heartrate.js
+++ src/compenents/Heartrate.js
@@ -1,7 +1,17 @@
-const RythmeCardiaque = 40 + Math.floor(Math.random() * (200 - 40));
-
 function Heartrate() {
-  return <div>{RythmeCardiaque}</div>;
+  //const {data} = props ; //mettre les valeurs de l'App dans le composant
+  var rythmeCardiaque = 40 + Math.floor(Math.random() * (200 - 40));
+  var pbBpm = ""
+  var description = " "
+  if (rythmeCardiaque<50 || rythmeCardiaque>150) {
+     pbBpm = "pbBpm";
+     description = rythmeCardiaque + " -> Danger";
+  }
+  else {
+     pbBpm ="bpmOk";
+     description = rythmeCardiaque + " -> Ok";
+  }
+  return <div class={pbBpm}> {description} </div>;
 }
 
 export default Heartrate;
